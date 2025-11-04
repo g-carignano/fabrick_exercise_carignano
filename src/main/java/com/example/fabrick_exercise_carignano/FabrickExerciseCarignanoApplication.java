e83aea817d8762d77a433b32d2b3d8b2cce7a9f0
g.carignano <g.carignano@reply.it>
2025-11-04 15:06:07 +0100

    Initial commit

---
+++ src/main/java/com/example/fabrick_exercise_carignano/FabrickExerciseCarignanoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fabrick_exercise_carignano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickExerciseCarignanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickExerciseCarignanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fabrick_exercise_carignano/FabrickExerciseCarignanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fabrick_exercise_carignano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickExerciseCarignanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
