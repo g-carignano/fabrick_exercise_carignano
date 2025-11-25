0caa4e2dff41393fad9ca255c9c07f17c4540dff
g.carignano <g.carignano@reply.it>
2025-11-25 14:48:04 +0100

    Updated ILocalTransactionService interface to inclued all necessary methods

--- src/main/java/com/example/fabrick_exercise_carignano/localservice/transaction/ILocalTransactionService.java
+++ src/main/java/com/example/fabrick_exercise_carignano/localservice/transaction/ILocalTransactionService.java
@@ -1,9 +1,15 @@
 package com.example.fabrick_exercise_carignano.localservice.transaction;
 
 import com.example.fabrick_exercise_carignano.localdto.TransactionDTO;
+import com.example.fabrick_exercise_carignano.localdto.TransactionListRequestDTO;
+import com.example.fabrick_exercise_carignano.localdto.TransactionResponseDTO;
 
 import java.util.List;
 
 public interface ILocalTransactionService {
     List<TransactionDTO> getAllInternalAccountTransactions();
+    List<TransactionDTO> getTransactionsByFilter(TransactionListRequestDTO transactionFilterDTO);
+    TransactionResponseDTO insertTransaction(TransactionDTO transactionDTO);
+    TransactionResponseDTO updateTransaction(TransactionDTO transactionDTO);
+    Boolean deleteTransaction(String guid);
 }
